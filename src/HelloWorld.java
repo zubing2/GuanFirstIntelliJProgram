7cca2f05fa538190601880f3e053b7babc13d95d
Zubin Guan <zubing2@nycstudents.net>
2022-09-21 19:31:13 -0400

    new print line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.print("Hi There");
+       System.out.println("Hello World!");
+       System.out.prinn("Hi There");
+       System.out.println("This is new");
     }
 }
